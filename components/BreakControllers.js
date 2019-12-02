9bd34314f8a3dc974a1800fd00e73b1c56ba9c32
Dumont Augustin <augustindmt@gmail.com>
2019-12-02 14:25:21 +0100

    add readMe

--- components/BreakControllers.js
+++ components/BreakControllers.js
@@ -1,37 +1,37 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class BreakController extends Component {
-    constructor(props) {
-        super(props);
-        this.handleBreakIncrement = this.handleBreakIncrement.bind(this);
-        this.handleBreakDecrement = this.handleBreakDecrement.bind(this);
+  constructor(props) {
+    super(props);
+    this.handleBreakIncrement = this.handleBreakIncrement.bind(this);
+    this.handleBreakDecrement = this.handleBreakDecrement.bind(this);
+  }
+  handleBreakIncrement() {
+    this.props.incrementBreakTime();
+    if (this.props.timerRunning === false) {
+      this.props.setCurrentTime(this.props.breakTime);
     }
-    handleBreakIncrement() {
-        this.props.incrementBreakTime();
-        if (this.props.timerRunning === false) {
-            this.props.setCurrentTime(this.props.breakTime);
-        }
-    }
+  }
 
-    handleBreakDecrement() {
-        this.props.decrementBreakTime();
-        if (this.props.timerRunning === false) {
-            this.props.setCurrentTime(this.props.breakTime);
-        }
+  handleBreakDecrement() {
+    this.props.decrementBreakTime();
+    if (this.props.timerRunning === false) {
+      this.props.setCurrentTime(this.props.breakTime);
     }
+  }
 
-    render() {
-        return (
-            <div className={"controller"}>
-                <p>BREAK</p>
-                <div className={"buttons"}>
-                    <button onClick={this.handleBreakIncrement}> + </button>
-                    <span> {Math.floor(this.props.breakTime / 60)} </span>
-                    <button onClick={this.handleBreakDecrement}> - </button>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className={"controller"}>
+        <p>BREAK</p>
+        <div className={"buttons"}>
+          <button onClick={this.handleBreakDecrement}> - </button>
+          <span> {Math.floor(this.props.breakTime / 60)} </span>
+          <button onClick={this.handleBreakIncrement}> + </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default BreakController;
